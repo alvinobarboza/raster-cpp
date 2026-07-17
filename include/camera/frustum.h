72751d55b9d3f692b180f228eeb20b15fcb19a21
Alvino N. C. Barboza <alvinobarboza@gmail.com>
2026-07-17 15:07:11 -0300

    setup...

---
+++ include/camera/frustum.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <array>
+
+#include "transforms/Vec3.h"
+
+constexpr size_t TOP_PLANE = 0;
+constexpr size_t BOTTOM_PLANE = 1;
+constexpr size_t LEFT_PLANE = 2;
+constexpr size_t RIGHT_PLANE = 3;
+constexpr size_t NEAR_PLANE = 4;
+constexpr size_t FAR_PLANE = 5;
+
+constexpr size_t PLANES = 6;
+
+class Plane {
+public:
+    float distance = 0.0f;
+    Vec3 normal = {};
+
+    Plane() = default;
+    Plane(const Vec3 &normal, const Vec3 &point);
+
+    [[nodiscard]] float signed_distance_to_point(const Vec3 &point) const;
+};
+
+class Frustum {
+public:
+    std::array<Plane, PLANES> planes;
+
+    Frustum() = default;
+
+    [[nodiscard]] bool is_inside_frustum(const Vec3 &point) const;
+    
+};
---
+++ include/colliders/sphere.h
@@ -0,0 +1,7 @@
+#pragma once
+
+class BoundingSphere {
+public:
+    float radius = 0.0f;
+
+};
