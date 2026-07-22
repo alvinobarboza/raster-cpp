7cdcab406a470172ba7fa7d4763f3f34a24b2658
Alvino N. C. Barboza <alvinobarboza@gmail.com>
2026-07-22 14:05:39 -0300

    fix - collision wiht raylib

--- include/transforms/Constants.h
+++ include/transforms/Constants.h
@@ -1,8 +1,8 @@
 #pragma once
 
-namespace Transforms {
-    constexpr float PI = 3.14159265358979323846;
-    constexpr float TAU = 2.0 * PI;
+namespace transforms {
+    constexpr float PI_R = 3.14159265358979323846;
+    constexpr float TAU = 2.0 * PI_R;
     constexpr float DEG_TO_RAD = TAU / 360.0f;
     constexpr float RAD_TO_DEG = 360.0f / TAU;
 }
