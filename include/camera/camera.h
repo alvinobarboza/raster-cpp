ddb8855259a1b22b2a5b5b1da2297a06b451bf89
Alvino N. C. Barboza <alvinobarboza@gmail.com>
2026-07-22 14:05:15 -0300

    Camera implementation

--- include/camera/camera.h
+++ include/camera/camera.h
@@ -24,8 +24,8 @@ public:
     bool render_depth = false;
     bool render_wireframe = false;
 
-    std::size_t width = 0;
-    std::size_t height = 0;
+    int width = 0;
+    int height = 0;
 
     float half_width = 0.0f;
     float half_height = 0.0f;
@@ -35,19 +35,32 @@ public:
     Frustum frustum = {};
 
     CameraRaster(
-        std::size_t width, std::size_t height,
+        int width, int height,
         float sensitivity, float fov,
-        Vec3 position, Vec3 rotation);
+        float near, float far,
+        const Vec3 &position, const Vec3 &rotation);
 
-    void update_frame_buffer_size();
+    void update_frame_buffer_size(int w, int h);
     void clear_frame_buffer();
     [[nodiscard]] Vec2 vertex_to_ndc(const Vec3 &vertex) const;
-    [[nodiscard]] Vec3 ndc_to_screen(const Vec2 &point) const;
+    [[nodiscard]] Vec2 ndc_to_screen(const Vec2 &point) const;
     [[nodiscard]] FullTriangle project_triangle(
         const Vertex &v1,
         const Vertex &v2,
         const Vertex &v3,
-        Texture &albedo,
-        Texture &normal,
-        Texture &specular) const;
+        TextureRaster *albedo,
+        TextureRaster *normal,
+        TextureRaster *specular) const;
+    [[nodiscard]] bool depth_pass(int x, int y, float depth);
+    void put_pixel(int x, int y, const Vec4 &color, float depth);
+    void move_forward_backwards(float unit);
+    void move_left_right(float unit);
+    void move_up_down(float unit);
+    void update_rotation(const Vec2 &rotation);
+    void toggle_view_lock();
+    void toggle_wireframe();
+    void toggle_render_depth();
+    void update_frustum();
 };
+
+static float fov_scaling(float angle);
---
+++ src/camera/camera.cpp
@@ -0,0 +1,224 @@
+#include "camera/camera.h"
+
+#include <cmath>
+
+#include "material/color_convertion.h"
+#include "transforms/Constants.h"
+
+CameraRaster::CameraRaster(
+    const int width, const int height,
+    const float sensitivity, const float fov,
+    const float near, const float far,
+    const Vec3 &position, const Vec3 &rotation)
+{
+    fov_angle = fov;
+    fov_scale = fov_scaling(fov);
+    z_near = near;
+    z_far = far;
+    this->sensitivity = sensitivity;
+    update_view = true;
+    render_depth = false;
+    render_wireframe = false;
+
+    transform.position = position;
+    transform.rotation = rotation;
+    transform.scale = {1,1,1};
+    transform.forward_direction = {0,0,1};
+
+    transform.update_transforms(true);
+    update_frame_buffer_size(width, height);
+}
+
+void CameraRaster::update_frame_buffer_size(const int w, const  int h)
+{
+    width = w;
+    height = h;
+    half_width = static_cast<float>(width) / 2;
+    half_height = static_cast<float>(height) / 2;
+    aspect_ratio = static_cast<float>(width) / static_cast<float>(height);
+    frame_buffer.resize(width*height, BLACK);
+    depth_buffer.resize(width*height, 0.0f);
+
+    update_frustum();
+}
+
+void CameraRaster::clear_frame_buffer()
+{
+    for (int i = 0; i < frame_buffer.size(); i++) {
+        frame_buffer[i] = BLACK;
+        depth_buffer[i] = 0.0f;
+    }
+}
+
+Vec2 CameraRaster::vertex_to_ndc(const Vec3 &vertex) const
+{
+    return
+    {
+        vertex.x * fov_scale / (vertex.z * aspect_ratio),
+        vertex.y * fov_scale / vertex.z,
+    };
+}
+
+Vec2 CameraRaster::ndc_to_screen(const Vec2 &point) const
+{
+    return
+    {
+        (point.x + 1.0f) * half_width,
+        (1.0f - point.y) * half_height
+    };
+}
+
+FullTriangle CameraRaster::project_triangle(
+    const Vertex &v1, const Vertex &v2, const Vertex &v3,
+    TextureRaster *albedo, TextureRaster *normal, TextureRaster *specular) const
+{
+    FullTriangle tri = {v1, v2, v3, albedo, normal, specular};
+
+    const Vec2 va = vertex_to_ndc(v1.point);
+    const Vec2 vb = vertex_to_ndc(v2.point);
+    const Vec2 vc = vertex_to_ndc(v3.point);
+
+    tri.screen_points[0] = ndc_to_screen(va);
+    tri.screen_points[1] = ndc_to_screen(vb);
+    tri.screen_points[2] = ndc_to_screen(vc);
+
+    tri.depth_z[0] = 1 / v1.point.z;
+    tri.depth_z[1] = 1 / v2.point.z;
+    tri.depth_z[2] = 1 / v3.point.z;
+
+    tri.points_projected[0] = v1.point * tri.depth_z[0];
+    tri.points_projected[1] = v2.point * tri.depth_z[1];
+    tri.points_projected[2] = v3.point * tri.depth_z[2];
+
+    tri.uvs_projected[0] = v1.uv * tri.depth_z[0];
+    tri.uvs_projected[1] = v2.uv * tri.depth_z[1];
+    tri.uvs_projected[2] = v3.uv * tri.depth_z[2];
+
+    tri.normals_rotated[0] = v1.normal;
+    tri.normals_rotated[1] = v2.normal;
+    tri.normals_rotated[2] = v3.normal;
+
+    tri.aabb = {
+        {
+            std::floor(
+                std::min(tri.screen_points[0].x, std::min(tri.screen_points[1].x, tri.screen_points[2].x))
+            ),
+            std::floor(
+                std::min(tri.screen_points[0].y, std::min(tri.screen_points[1].y, tri.screen_points[2].y))
+            )
+        },
+        {
+            std::ceil(
+                std::max(tri.screen_points[0].x, std::max(tri.screen_points[1].x, tri.screen_points[2].x))
+            ),
+            std::ceil(
+                std::max(tri.screen_points[0].y, std::max(tri.screen_points[1].y, tri.screen_points[2].y))
+            )
+        }
+    };
+
+    return tri;
+}
+
+bool CameraRaster::depth_pass(const int x, const int y, const float depth)
+{
+    if (x < 0 || x >= width || y < 0 || y >= height) return false;
+
+    const int index = y * width + x;
+    if (depth_buffer[index] > depth) return false;
+
+    depth_buffer[index] = depth;
+    return true;
+}
+
+void CameraRaster::put_pixel(const int x, const int y, const Vec4 &color, const float depth)
+{
+    const int index = y * width + x;
+
+    if (index > depth_buffer.size()) return;
+
+    if (render_depth)
+    {
+        const float c = 255.0f * depth;
+        const Vec4 color_f = {c,c,c,1};
+        frame_buffer[index] = color_convertion::vec4_to_color(color_f);
+        return;
+    }
+    frame_buffer[index] = color_convertion::vec4_to_color(color);
+}
+
+void CameraRaster::move_forward_backwards(const float unit)
+{
+    const Vec3 direction = transform.forward_direction * transform.rotation_matrix;
+    const Vec3 normalized_dir = direction.normalized();
+
+    transform.position += normalized_dir * unit;
+    transform.update_transforms(true);
+}
+
+void CameraRaster::move_left_right(const float unit)
+{
+    const Vec3 direction = transform.forward_direction * transform.rotation_matrix;
+    const Vec3 cross_up = direction.cross({0.0f, 0.1f, 0.0f});
+    const Vec3 normalized_dir = cross_up.normalized();
+
+    transform.position += normalized_dir * unit;
+    transform.update_transforms(true);
+}
+
+void CameraRaster::move_up_down(const float unit)
+{
+    transform.position.y += unit;
+    transform.update_transforms(true);
+}
+
+void CameraRaster::update_rotation(const Vec2 &rotation)
+{
+    if (!update_view) return;
+
+    transform.rotation.x -= rotation.y * sensitivity;
+    transform.rotation.y += rotation.x * sensitivity;
+
+    if (transform.rotation.x > 89) transform.rotation.x = 89;
+    if (transform.rotation.x < -89) transform.rotation.x = -89;
+
+    transform.update_transforms(true);
+}
+
+void CameraRaster::toggle_render_depth()
+{
+    render_depth = !render_depth;
+}
+
+void CameraRaster::toggle_view_lock()
+{
+    update_view = !update_view;
+}
+
+void CameraRaster::toggle_wireframe()
+{
+    render_wireframe = !render_wireframe;
+}
+
+void CameraRaster::update_frustum()
+{
+    const Vec3 cam_front = transform.forward_direction;
+    const Vec3 cam_right = {1.0f, 0.0f, 0.0f};
+    const Vec3 cam_up = {0.0f, 1.0f, 0.0f};
+    const Vec3 cam_pos = {};
+
+    const float half_v_side = z_far * std::tan(fov_angle * transforms::DEG_TO_RAD * 0.5f);
+    const float half_h_side = half_v_side * aspect_ratio;
+    const Vec3 cam_front_scaled = cam_front * z_far;
+
+    frustum.planes[NEAR_PLANE] = {cam_pos + cam_front * z_near, cam_front};
+    frustum.planes[FAR_PLANE] = {cam_pos + cam_front_scaled, -cam_front};
+    frustum.planes[RIGHT_PLANE] = {cam_pos, (cam_front_scaled + cam_right * half_h_side).cross(cam_up)};
+    frustum.planes[LEFT_PLANE] = {cam_pos, cam_up.cross(cam_front_scaled - cam_right * half_h_side)};
+    frustum.planes[TOP_PLANE] = {cam_pos, (cam_front_scaled - cam_up * half_v_side).cross(cam_right)};
+    frustum.planes[BOTTOM_PLANE] = {cam_pos, cam_right.cross(cam_front_scaled - cam_up * half_v_side)};
+}
+
+float fov_scaling(const float angle) {
+    return 1 / std::tan(angle*transforms::DEG_TO_RAD/2);
+}
