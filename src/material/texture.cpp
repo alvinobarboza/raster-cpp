e70782bb41ef8664dd660916c5845b22ec2e5e76
Alvino N. C. Barboza <alvinobarboza@gmail.com>
2026-08-22 17:09:55 -0300

    upsidedown sampling

--- src/material/texture.cpp
+++ src/material/texture.cpp
@@ -4,8 +4,9 @@
 
 Vec4 TextureRaster::texel_color(const Vec2 &uv) const
 {
+    const float inverted_y = 1.0f - uv.y;
     const int w = static_cast<int>(std::floor(uv.x * f_width)) & width_mask;
-    const int h = static_cast<int>(std::floor(uv.y * f_height)) & height_mask;
+    const int h = static_cast<int>(std::floor(inverted_y * f_height)) & height_mask;
     return buffer[h * width + w];
 }
 
